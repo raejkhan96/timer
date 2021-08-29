d2bce2eaa483a32aaba581e23f8cb4a250e325d7
Rae J. Khan <raekhan18@gmail.com>
2021-08-29 17:58:21 +0000

    update

--- timer.js
+++ timer.js
@@ -4,21 +4,21 @@ const alarm = function(value) {
 
   if (typeof value === 'number' && value > 0) {
 
-    setTimeout(function() { 
+    setTimeout(function() {
       // create beep sound
       process.stdout.write('\x07');
       // . to confirm beep sound
-      process.stdout.write('.'); 
+      process.stdout.write('.');
       console.log('\n');
-    }, value*1000);
+    }, value * 1000);
   
   }
-  
-}
+
+};
 
 for (let i = 2; i < (args.length); i++) {
   
-  alarm(+args.slice(i, i+1));
+  alarm(+args.slice(i, i + 1));
   
 }
 
